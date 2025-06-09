a5a0886cab39da727ba0d954ff2cbc81fd493808
Nicholo Dacanay <itsnicholoxd@gmail.com>
2025-06-09 15:40:50 +0800

    updated sdk version to 53 and updated the dependencies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome bro</ThemedText>
+        <ThemedText type="title">Welcome to Powerboard</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
